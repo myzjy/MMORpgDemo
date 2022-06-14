93cc292881110be97207f435d6689c89d811cd8b
zjy <2910553808@qq.com>
2022-06-15 00:12:11 +0800

    ui

--- Assets/Script/Framework/UI/UIBaseModule.cs
+++ Assets/Script/Framework/UI/UIBaseModule.cs
@@ -1,4 +1,8 @@
-﻿namespace Script.Framework.UI
+﻿using System.Collections;
+using Script.Framework.AssetBundle;
+using UnityEngine;
+
+namespace Script.Framework.UI
 {
     /// <summary>
     /// UI基础模型
@@ -19,12 +23,24 @@
             return null;
         }
 
+        public virtual string PrefabName()
+        {
+            return "";
+        }
+
         /// <summary>
         /// 行为事件回调
         /// </summary>
         /// <param name="notification"></param>
         public virtual void NotificationHandler(UINotification notification)
         {
+        }
+
+        public IEnumerator LoadAsset()
+        {
+            var load = AssetBundleManager.Instance.LoadAssetAsync(PrefabName(), typeof(GameObject));
+            yield return load;
+
         }
     }
 }
